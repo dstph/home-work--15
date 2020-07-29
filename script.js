617577e1e62caf9e3130dae9a023d4bdaf088b74
dstph <genyadstph@gmail.com>
2020-07-29 17:38:20 +0400

    second task

--- script.js
+++ script.js
@@ -1,84 +1,83 @@
-// задание 1
 
-// Добавить всем массивам геттер – render, 
-// который будет выводить массив в документ,
-// в виде таблицы (номер ячейки и значение ячейки).
+// let	table = document.createElement('table'),
+// 	thead = document.createElement('thead'),
+// 	tr = document.createElement('tr'),
+// 	th1 = document.createElement('th'),
+// 	th2 = document.createElement('th');
 
-let	table = document.createElement('table'),
-	thead = document.createElement('thead'),
-	tr = document.createElement('tr'),
-	th1 = document.createElement('th'),
-	th2 = document.createElement('th');
+// th1.innerText = "number";
+// th2.innerText = "Value";
 
-th1.innerText = "number";
-th2.innerText = "Value";
+// document.body.appendChild(table);
+// table.appendChild(thead);
+// thead.appendChild(tr);
+// tr.appendChild(th1);
+// tr.appendChild(th2);
 
-document.body.appendChild(table);
-table.appendChild(thead);
-thead.appendChild(tr);
-tr.appendChild(th1);
-tr.appendChild(th2);
-
-Array.prototype.render = function  render(){
-	for(i=0; i<this.length; i++){
-		let tr_in = document.createElement('tr'),
-		    td_number = document.createElement('td'),
-		    td_value = document.createElement('td');
+// Array.prototype.render = function  render(){
+// 	for(i=0; i<this.length; i++){
+// 		let tr_in = document.createElement('tr'),
+// 		    td_number = document.createElement('td'),
+// 		    td_value = document.createElement('td');
 		
-		td_number.innerText = i;
-		td_value.innerText = this[i];
+// 		td_number.innerText = i;
+// 		td_value.innerText = this[i];
 
-		table.appendChild(tr_in);
-		tr_in.appendChild(td_number);
-		tr_in.appendChild(td_value);
+// 		table.appendChild(tr_in);
+// 		tr_in.appendChild(td_number);
+// 		tr_in.appendChild(td_value);
 
 
+// 	}
+// };
+
+// let pussy = ["Benya", "Kotleta", "Eva", "Lucifer"];
+// pussy.render();
+
+// console.dir(pussy);
+
+
+let data = {
+	get model(){
+		return  this.modelValue;
+	},
+	set model(value){
+		if(this.modelValue){
+			this.str = this.modelValue.split(value);
+			this.modelValue = {symbol: value, count: this.str.length};
+		} else{
+			this.modelValue = value;
+		}
 	}
-};
-
-let pussy = ["Benya", "Kotleta", "Eva", "Lucifer"];
-pussy.render();
-
-console.dir(pussy);
+}
 
 
+data.model = 'never sleep';
 
-/*задание 2
+console.log(data.model);
 
-У объекта data создать свойство геттер `model` и 
-сеттер `model`. Которое обрабатывает  данные после 
-двух обращений.
+data.model = ' ';
 
-При первом обращении сохраняется строка.
- Например: `data.model = 'Hello, how, are you?'`
+console.log(data.str);
+console.dir(data);
+console.log(data.model);
 
-При втором обращении к сеттеру происходит нарезание
- строки по переданному символу и сохранение результата
-  в свойство str. Например: `data.model = ","`
 
-`console.log(data.str) -> ['Hello', ' how', ' are you?']`
 
-----------------------
 
-Геттер `data.model` возвращает символы, 
-которые были удалены (при втором вызове сеттера) 
-и их количество в виде объекта.
+// задание 3
 
-`console.log(data.model); -> { symbol: ',', count: 2 }`
+// Создать сеттер `model` (не связывать с задачей №2),
+//  который принимает объект и выполняет `update` всех 
+//  существующих свойств в текущем объекте.
 
-задание 3
+// `obj = {x:10, y: 20, p: 15};`
 
-Создать сеттер `model` (не связывать с задачей №2),
- который принимает объект и выполняет `update` всех 
- существующих свойств в текущем объекте.
+// `obj.model = { x: { value: 33, operation: '*' }, 
+// 			   z: { value: 75, operation: '+' }, 
+// 			   p: { value: 4, operation: '%' } }`
 
-`obj = {x:10, y: 20, p: 15};`
-
-`obj.model = { x: { value: 33, operation: '*' }, 
-			   z: { value: 75, operation: '+' }, 
-			   p: { value: 4, operation: '%' } }`
-
-`console.log(obj); -> {x:330, y: 20, z: 75, p: 3}`*/
+// `console.log(obj); -> {x:330, y: 20, z: 75, p: 3}`*/
 
 
 
